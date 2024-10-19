8216bc72b5ed66fbfac7e8404728e6b4f6737655
Andres Arcila <andresarcilaa2@me.com>
2024-10-19 16:02:38 -0500

    exponiendo los componentes

--- vueloFramework/servers/bun.ts
+++ vueloFramework/servers/bun.ts
@@ -3,6 +3,9 @@ import { createVueloApp } from "../createVueloApp";
 import { resolveRouteComponent } from "../router";
 import getTemplate from "../utils/template";
 import type { VueloConfig } from "../interfaces/vueloConfig";
+import fs from 'fs'; // Asegúrate de importar el módulo fs para trabajar con el sistema de archivos
+import path from 'path'; // Asegúrate de importar el módulo path para manejar rutas
+
 
 export default function BunServer(
     vite: ViteDevServer,
@@ -14,16 +17,25 @@ export default function BunServer(
         const url = new URL(req.url);
   
         // manejar rutas de islas
-        if (url.pathname.startsWith("/api/")) {
-          for (const island of components.islands) {
-            const name = "/api/islands/" + island.name;
-            const path = island.path;
-            console.log(url.pathname, island);
-  
-            if (url.pathname === name) {
-              return new Response(name);
-            }
-          }
+        if (url.pathname.startsWith("/api/islands/")) {
+            for (const island of components.islands) {
+                const name = "/api/islands/" + island.name;
+                const filePath = path.join(__dirname, "../../",island.path); // Ruta al archivo de la isla
+                console.log(url.pathname, island);
+      
+                if (url.pathname === name) {
+                  try {
+                    // Leer el contenido del archivo
+                    const fileContent = await fs.promises.readFile(filePath, 'utf-8');
+                    return new Response(fileContent, {
+                      headers: { "Content-Type": "text/plain" }, // Cambia el Content-Type según sea necesario
+                    });
+                  } catch (error) {
+                    console.error("Error reading file:", error);
+                    return new Response("Internal Server Error", { status: 500 });
+                  }
+                }
+              }
           // Si ninguna ruta de isla coincide, puedes retornar un 404 o un mensaje
           return new Response("Not Found", { status: 404 });
         } else {
